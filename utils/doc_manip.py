6be5b3dc5a0be5fcf3642e75dbefb4e997f8253a
ramaneswaran <ramannewdelhi44@gmail.com>
2020-06-06 00:30:17 +0530

    Add image manip

---
+++ utils/doc_manip.py
@@ -0,0 +1,21 @@
+from pdf2image import convert_from_path
+
+class Manip:
+    '''
+    Class to deals with docs
+    '''
+    def __init__(self):
+        pass
+
+    def save_as_image(self, path):
+        '''
+        This function return path of saved image
+        '''
+
+        pages = convert_from_path(path)
+
+        image = page[0]
+
+        image.save('./data/tmp/img.jpg')
+
+        return './data/tmp/img.jpg'
